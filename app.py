429a7a23a3155a30de254938d81723d1b7e79731
Pratheek Athreya <pratheek@MacBook-Pro.local>
2021-06-16 18:03:37 -0700

    Updating code

--- app.py
+++ app.py
@@ -3,31 +3,32 @@ from flask import Flask, request, abort, make_response, jsonify
 from ecdsa import SigningKey, NIST256p
 import json
 app = Flask(__name__)
+session = {}
 
 @app.route('/get_api_key')
 def get_api_key():
     sk = SigningKey.generate(curve = NIST256p) # generates signing key with base 64 encoding
-    sk_string = sk.to_string() # serializes signing key in the form of bytes
     
+    session['vk'] = sk.verifying_key
+    sk_string = sk.to_string() # serializes signing key in the form of bytes
+
     return sk_string.hex() # we return the output in the form of string
 
 @app.route('/verify_api_key',methods=['GET','POST'])
 def verify_api_key():
     '''
-    The module verifies the data field which is sent as a parameter using ecdsa keys, if it is verified it returns a
-    basic calculator module involving just 2 operands
+    The module verifies the data field which is sent as a parameter using ecdsa keys, if it is verified it performs a
+    basic calculator functionality involving just 2 operands
     '''
     try:
         content = request.json
+        # converting json data to string form and then bytes
         str_data = str(content['data'])
         bytes_data = str.encode(str_data)
-    
-        ecdsa_signature = bytes.fromhex(content['signature']) # convert ecdsa signature to bytes form
+        signature = bytes.fromhex(content['signature'])
+        vk = session['vk']
         
-        sk = SigningKey.from_string(ecdsa_signature,curve = NIST256p) # get the signing key back
-        signature = sk.sign(bytes_data)
-        vk = sk.verifying_key
-        assert vk.verify(signature, bytes_data)
+        assert vk.verify(signature,bytes_data)  # verifying the signature
         valid_operators = {'+','-','/','*','%','**','//'}
         val1 = str(content['data']['v1']) # operand1
         val2 = str(content['data']['v2']) # operand2
--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import requests
 import sys
+
+from ecdsa import SigningKey, NIST256p
 '''
 In this script we first fetch ecdsa key from an api and pass it on to the api to verify it
 '''
@@ -12,7 +14,15 @@ if __name__ == '__main__':
     op = sys.argv[3]
     url_1 = 'http://localhost:5000/get_api_key'
     response = requests.get(url_1)
-    url_2 = 'http://localhost:5000/verify_api_key'
-    body = {'data':{'v1':val1,'v2':val2,'op':op,},'signature': response.text}
+    ecdsa_key = bytes.fromhex(response.text)
+ 
+    sk = SigningKey.from_string(ecdsa_key,curve = NIST256p) 
+    url_2 = 'http://localhost:5000/verify_api_key' # call the 2nd api for verification
+    body = {'data':{'v1':int(val1),'v2':int(val2),'op':op}}
+    str_data = str(body['data'])
+    bytes_data = str.encode(str_data)
+    signature = sk.sign(bytes_data) # we sign the data and send it to the server
+    body['signature'] = signature.hex()
+
     r = requests.post(url_2,json = body)
     print(r.text)
